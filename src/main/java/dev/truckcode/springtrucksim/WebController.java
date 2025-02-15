7c146f6e028fdaafe8b79a66dc9f8ddc9d37c00c
Robert Grzazek <rgrzazek@gmail.com>
2025-02-16 00:28:43 +1100

    Basic blurb, a few skeleton pages, experiment with fragments

--- src/main/java/dev/truckcode/springtrucksim/WebController.java
+++ src/main/java/dev/truckcode/springtrucksim/WebController.java
@@ -5,11 +5,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class WebController {
 
   @GetMapping("/")
   public String home(Model model) {
     model.addAttribute("message", "Welcome to the Spring Truck Sim!");
     return "home"; // Thymeleaf will look for home.html in src/main/resources/templates/
   }
+
+  @GetMapping("/why")
+  public String why(Model model) {
+    return "commentary";
+  }
+
+  @GetMapping("/about_me")
+  public String aboutMe(Model model) {
+    return "about_me";
+  }
+
+  @GetMapping("/plans")
+  public String plans(Model model) {
+    return "plans";
+  }
+
+  @GetMapping("/sim")
+  public String sim(Model model) {
+    return "simulator";
+  }
+
 }
