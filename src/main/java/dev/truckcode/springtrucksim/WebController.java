a3f2413aca7aed6eac44807fedee80454e8b5fdf
Robert Grzazek <rgrzazek@gmail.com>
2026-05-26 17:15:56 +1000

    Simplify site map. Add blurb

--- src/main/java/dev/truckcode/springtrucksim/WebController.java
+++ src/main/java/dev/truckcode/springtrucksim/WebController.java
@@ -1,35 +1,23 @@
 package dev.truckcode.springtrucksim;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
 
   @GetMapping("/")
-  public String home(Model model) {
-    model.addAttribute("message", "Welcome to the Spring Truck Sim!");
-    return "home"; // Thymeleaf will look for home.html in src/main/resources/templates/
-  }
-
-  @GetMapping("/why")
-  public String why(Model model) {
-    return "commentary";
+  public String home() {
+    return "home";
   }
 
   @GetMapping("/about_me")
-  public String aboutMe(Model model) {
+  public String aboutMe() {
     return "about_me";
   }
 
-  @GetMapping("/plans")
-  public String plans(Model model) {
-    return "plans";
-  }
-
   @GetMapping("/sim")
-  public String sim(Model model) {
+  public String sim() {
     return "simulator";
   }
 
